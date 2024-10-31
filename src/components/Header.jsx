6ac6347f0f3054c5d1dcbba4591f9d18172f85a3
leorrusso <leonardoramosrusso@gmail.com>
2024-10-30 23:25:59 -0300

    git ignore case settings changed

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = ({ handleLogout }) => {
+  return (
+    <header className="bg-purple-500 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h1 className="text-3xl font-bold">Power Apps Tools</h1>
+
+        {/* Navbar Menu */}
+        <nav className="space-x-4">
+          <Link 
+            to="/users" 
+            className="text-white px-3 py-2 rounded text-sm font-semibold"
+          >
+            Users
+          </Link>
+          <Link 
+            to="/snippets" 
+            className="text-white px-3 py-2 rounded text-sm font-semibold"
+          >
+            Code Snippets
+          </Link>
+        </nav>
+
+        {/* Logout Button */}
+        <button 
+          onClick={handleLogout}
+          className="bg-red-600 text-white px-3 py-1 rounded text-sm font-semibold ml-4"
+        >
+          Log out
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
