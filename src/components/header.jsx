f739350c20363d328824d900fe04333b4e671bce
leorrusso <leonardoramosrusso@gmail.com>
2024-10-30 22:53:11 -0300

    header appearance changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = ({ handleLogout }) => {
   return (
-    <header className="bg-blue-600 text-white py-4">
+    <header className="bg-purple-500 text-white py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
         <h1 className="text-3xl font-bold">Power Apps Tools</h1>
 
@@ -11,13 +11,13 @@ const Header = ({ handleLogout }) => {
         <nav className="space-x-4">
           <Link 
             to="/users" 
-            className="bg-gray-800 text-white px-3 py-1 rounded text-sm font-semibold"
+            className="text-white px-3 py-2 rounded text-sm font-semibold"
           >
             Users
           </Link>
           <Link 
             to="/snippets" 
-            className="bg-gray-800 text-white px-3 py-1 rounded text-sm font-semibold"
+            className="text-white px-3 py-2 rounded text-sm font-semibold"
           >
             Code Snippets
           </Link>
